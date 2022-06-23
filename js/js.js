213f3bd85b5a2130bfbc8e07e6bce10ffc9b8a3c
IbrahimCem01 <ibrahim.cem.oksuz@icloud.com>
2022-06-23 20:52:58 +0300

    Video eklendi. Jquery çalışması yapıldı.

---
+++ js/js.js
@@ -0,0 +1,12 @@
+//$(seçici).fonksiyon();
+//dolar option + 4
+$(document).ready(function(){
+
+    $("#button1").click(function(){
+        var isim= "İbrahim Cem";
+        alert("Merhaba "+ isim);
+    })
+    $("#button2").click(function(){
+        alert("Merhaba İbrahim 2");
+    })
+});
